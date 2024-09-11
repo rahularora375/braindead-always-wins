a3ad1cd9497f118c4e9ff480eed18343e3d81a3d
TmoNNNomT <hi.the.world.says.hello@gmail.com>
2024-09-11 23:41:50 +0530

    - Added functions to read and write a CommitObject - Added a ProblemsFaced.txt file to track challenges - Added a bunch of TODOs for next steps and potential design changes.

--- src/main/java/org/example/commands/Init.java
+++ src/main/java/org/example/commands/Init.java
@@ -10,15 +10,13 @@ public class Init {
     }
 
     public static void execute(String[] args) throws Exception {
-        /* TODO: Create a .baw directory if not exists
-        *  TODO: Initialize CommitHead to point null and save in .baw
-        *  TODO: Add sub-directory to store all commits in local
+        /* TODO:
+            - Create a .baw directory if not exists
+            - Initialize CommitHead to point null and save in .baw
+            - Add sub-directory to store all commits in local
         */
 
         if(!validateArgs(args))
             throw new Exception("Unnecessary arguments provided!");
-
-
-
     }
 }
